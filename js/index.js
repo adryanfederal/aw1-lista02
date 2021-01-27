8567608b32fddd39856908bb67eddf9e972aa770
Adryan Alencar <adryan.alencar@aluno.ifsp.edu.br>
2021-01-27 14:29:55 -0300

    finish him

--- js/index.js
+++ js/index.js
@@ -21,29 +21,29 @@ var month = [
  * Exercicio 01
  */
 
-var exercice_one_input = document.getElementById('exercice-one-input');
-var exercice_one_validation = document.getElementById('exercice-one-validation');
-var exercice_one_button = document.getElementById('exercice-one-button');
+var exercise_one_input = document.getElementById('exercise-one-input');
+var exercise_one_validation = document.getElementById('exercise-one-validation');
+var exercise_one_button = document.getElementById('exercise-one-button');
 
-exercice_one_input.addEventListener('keyup', (e) => {
+exercise_one_input.addEventListener('keyup', (e) => {
 
     if (!e.target.checkValidity()) {
-        exercice_one_validation.innerText = e.target.validationMessage;
-        exercice_one_validation.className = "span-danger";
+        exercise_one_validation.innerText = e.target.validationMessage;
+        exercise_one_validation.className = "span-danger";
     } else {
-        exercice_one_validation.innerText = "Número aceito";
-        exercice_one_validation.className = "span-success";
+        exercise_one_validation.innerText = "Número aceito";
+        exercise_one_validation.className = "span-success";
     }
 })
 
-exercice_one_button.addEventListener('click', () => {
+exercise_one_button.addEventListener('click', () => {
 
-    if (!exercice_one_input.checkValidity()) {
-        exercice_one_validation.innerText = exercice_one_input.validationMessage;
-        exercice_one_validation.className = "span-danger";
+    if (!exercise_one_input.checkValidity()) {
+        exercise_one_validation.innerText = exercise_one_input.validationMessage;
+        exercise_one_validation.className = "span-danger";
     } else {
-        exercice_one_validation.innerText = "Número aceito";
-        exercice_one_validation.className = "span-success";
+        exercise_one_validation.innerText = "Número aceito";
+        exercise_one_validation.className = "span-success";
     }
 })
 
@@ -51,12 +51,12 @@ exercice_one_button.addEventListener('click', () => {
  * Exercicio 02
  */
 
-var exercice_two_input = document.getElementById("exercice-two-input");
-var exercice_two_validation = document.getElementById("exercice-two-validation");
-var exercice_two_response = document.getElementById("exercice-two-response");
-var exercice_two_button = document.getElementById('exercice-two-button');
+var exercise_two_input = document.getElementById("exercise-two-input");
+var exercise_two_validation = document.getElementById("exercise-two-validation");
+var exercise_two_response = document.getElementById("exercise-two-response");
+var exercise_two_button = document.getElementById('exercise-two-button');
 
-exercice_two_input.addEventListener("keyup", (e) => {
+exercise_two_input.addEventListener("keyup", (e) => {
 
     var value = e.target.value
     var data_splited = value.split("/")
@@ -66,18 +66,18 @@ exercice_two_input.addEventListener("keyup", (e) => {
         var date = new Date(`${data_splited[2]}/${data_splited[1]}/${data_splited[0]}`)
 
         if (isNaN(date.getTime())) {
-            exercice_two_validation.innerText = "A data inserida é inválida."
-            exercice_two_validation.className = "span-danger";
+            exercise_two_validation.innerText = "A data inserida é inválida."
+            exercise_two_validation.className = "span-danger";
             return;
         }
-        exercice_two_validation.innerText = ""
-        exercice_two_response.innerText = `${date.getDay()} de ${month[date.getMonth()]} de ${date.getFullYear()}`;
+        exercise_two_validation.innerText = ""
+        exercise_two_response.innerText = `${date.getDay()} de ${month[date.getMonth()]} de ${date.getFullYear()}`;
     }
 
 })
 
-exercice_two_button.addEventListener("click", (e) => {
-    var value = exercice_two_input.value
+exercise_two_button.addEventListener("click", (e) => {
+    var value = exercise_two_input.value
     var data_splited = value.split("/")
 
     if (data_splited.length > 2) {
@@ -85,12 +85,12 @@ exercice_two_button.addEventListener("click", (e) => {
         var date = new Date(`${data_splited[2]}/${data_splited[1]}/${data_splited[0]}`)
 
         if (isNaN(date.getTime())) {
-            exercice_two_validation.innerText = "A data inserida é inválida."
-            exercice_two_validation.className = "span-danger";
+            exercise_two_validation.innerText = "A data inserida é inválida."
+            exercise_two_validation.className = "span-danger";
             return;
         }
-        exercice_two_validation.innerText = ""
-        exercice_two_response.innerText = `${date.getDay()} de ${month[date.getMonth()]} de ${date.getFullYear()}`;
+        exercise_two_validation.innerText = ""
+        exercise_two_response.innerText = `${date.getDay()} de ${month[date.getMonth()]} de ${date.getFullYear()}`;
     }
 })
 
@@ -98,69 +98,84 @@ exercice_two_button.addEventListener("click", (e) => {
  * Exercicio 03
  */
 
-var exercice_tree_heigth = document.getElementById('exercice-tree-heigth');
-var exercice_tree_wight = document.getElementById('exercice-tree-wight');
-var exercice_tree_radio_male = document.getElementById('exercice-tree-male');
-var exercice_tree_radio_female = document.getElementById('exercice-tree-female');
-var exercice_tree_validation = document.getElementById('exercice-tree-validation');
-var exercice_tree_response = document.getElementById('exercice-tree-response');
-var exercice_tree_button = document.getElementById('exercice-tree-button');
+var exercise_three_heigth = document.getElementById('exercise-three-heigth');
+var exercise_three_wight = document.getElementById('exercise-three-wight');
+var exercise_three_radio_male = document.getElementById('exercise-three-male');
+var exercise_three_radio_female = document.getElementById('exercise-three-female');
+var exercise_three_validation = document.getElementById('exercise-three-validation');
+var exercise_three_response = document.getElementById('exercise-three-response');
+var exercise_three_button = document.getElementById('exercise-three-button');
 
 function validation() {
-    if (!(exercice_tree_radio_female.checked || exercice_tree_radio_male.checked)) {
+    if (!(exercise_three_radio_female.checked || exercise_three_radio_male.checked)) {
         return -1
     }
 
-    if (!(exercice_tree_heigth.value != "" || exercice_tree_wight != "")) {
+    if (!(exercise_three_heigth.value != "" || exercise_three_wight != "")) {
         return 0
     }
 
     return 1
 }
 
-exercice_tree_button.addEventListener('click', (e) => {
+exercise_three_button.addEventListener('click', (e) => {
     let result = validation();
 
     switch (result) {
         case -1:
-            exercice_tree_validation.innerText = "Selecione o sexo";
-            exercice_tree_validation.className = "span-danger";
+            exercise_three_validation.innerText = "Selecione o sexo";
+            exercise_three_validation.className = "span-danger";
             break;
         case 0:
-            exercice_tree_validation.innerText = "Insira o seu peso e altura";
-            exercice_tree_validation.className = "span-danger";
+            exercise_three_validation.innerText = "Insira o seu peso e altura";
+            exercise_three_validation.className = "span-danger";
             break;
         case 1:
-            exercice_one_validation.innerText = "";
+            exercise_one_validation.innerText = "";
             break;
     }
 
-    let imc = Number(exercice_tree_wight.value) / (Number(exercice_tree_heigth.value) * Number(exercice_tree_heigth.value))
+    let imc = Number(exercise_three_wight.value) / (Number(exercise_three_heigth.value) * Number(exercise_three_heigth.value))
 
     if (result != 1) {
         return;
     }
 
-    if (exercice_tree_radio_female.checked) {
+    if (exercise_three_radio_female.checked) {
         if (imc < 19) {
-            exercice_tree_response.innerText = "Abaixo do ideal";
+            exercise_three_response.innerText = "Abaixo do ideal";
 
         } else if (imc >= 19 && imc <= 25.8) {
-            exercice_tree_response.innerText = "Peso ideal";
+            exercise_three_response.innerText = "Peso ideal";
 
         } else if (imc > 25.8) {
-            exercice_tree_response.innerText = "Acima do peso ideal";
+            exercise_three_response.innerText = "Acima do peso ideal";
         }
     } else {
         if (imc < 20.7) {
-            exercice_tree_response.innerText = "Abaixo do ideal";
+            exercise_three_response.innerText = "Abaixo do ideal";
 
         } else if (imc >= 20.7 && imc <= 26.4) {
-            exercice_tree_response.innerText = "Peso ideal";
+            exercise_three_response.innerText = "Peso ideal";
 
         } else if (imc > 26.4) {
-            exercice_tree_response.innerText = "Acima do peso ideal";
+            exercise_three_response.innerText = "Acima do peso ideal";
         }
     }
+
+})
+
+/**
+ * Exercicio 04
+ */
+
+var exercise_four_input = document.getElementById('exercise-four-input');
+var exercise_four_button = document.getElementById('exercise-four-button');
+var exercise_four_response = document.getElementById('exercise-four-response');
+
+exercise_four_button.addEventListener('click', () => {
+    exercise_four_response.innerText = '';
+
+    exercise_four_response.innerText = exercise_four_input.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 
 })
